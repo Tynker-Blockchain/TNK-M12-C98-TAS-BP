b134a638ea20055de93e3206412e70a72ccf3371
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-10-20 14:02:59 +0530

    Add files via upload

---
+++ TA3/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, redirect, session
+import os
+from time import time
+from wallet import Wallet
+from wallet import Account
+
+STATIC_DIR = os.path.abspath('static')
+
+app = Flask(__name__, static_folder=STATIC_DIR)
+app.use_static_for_root = True
+
+myWallet =  Wallet()
+account = None
+
+@app.route("/", methods= ["GET", "POST"])
+def home():
+    global myWallet, account
+    isConnected = myWallet.checkConnection()
+    balance = "No Balance"    
+    if(account):
+        balance = myWallet.getBalance(account.address)
+       
+    return render_template('index.html', isConnected=isConnected,  account= account, balance = balance)
+
+
+@app.route("/makeTransaction", methods = ["GET", "POST"])
+def makeTransaction():
+    global myWallet, account
+
+    sender = request.form.get("senderAddress")
+    receiver = request.form.get("receiverAddress")
+    amount = request.form.get("amount")
+
+    senderType = 'ganache'
+    if(sender == account.address):
+        senderType = 'newAccountAddress'
+
+    # Store response as tnxHash
+    myWallet.makeTransactions(sender, receiver, amount, senderType, account.privateKey)
+    # Call addTransactionHash method to store the transaction
+    
+    return redirect("/")
+
+
+@app.route("/createAccount", methods= ["GET", "POST"])
+def createAccount(): 
+    global myWallet, account
+    account = Account()
+    return redirect("/")
+
+if __name__ == '__main__':
+    app.run(debug = True, port=4000)
